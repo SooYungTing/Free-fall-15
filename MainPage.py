b97dbde8af76cb11ecbe0aca0c84b0fad8d6c443
Administrator <Administrator@YTSoo2024.local>
2023-03-15 20:53:36 +0800

    Sizes of the buttons set but it keeps going down whenever the back button is clicked from theory page

--- MainPage.py
+++ MainPage.py
@@ -16,27 +16,29 @@ class MainPageGUI:
 
         # Create label to display welcome message
         welcome_message = "Welcome to the Free-Fall 15 simulation!"
-        self.welcome_label = tk.Label(self.root, text=welcome_message, font=("Times New Roman", 18, "bold"), fg="Yellow", bg="SystemTransparent")
+        self.welcome_label = tk.Label(self.root, text=welcome_message, font=("Times New Roman", 40, "bold"), fg="Yellow", bg="SystemTransparent")
         self.welcome_label.pack(pady=20)
 
         # Create button to open theory file
-        theory_button = tk.Button(self.root, text="Theory", command=self.open_theory, font=("Times New Roman", 16))
+        theory_button = tk.Button(self.root, text="Theory", command=self.open_theory, font=("Times New Roman", 16), width=16, height=2)
         theory_button.pack(pady=10)
 
         # Create button to open simulation file
-        simulation_button = tk.Button(self.root, text="Simulation", command=self.open_simulation, font=("Times New Roman", 16))
+        simulation_button = tk.Button(self.root, text="Simulation", command=self.open_simulation, font=("Times New Roman", 16), width=16, height=2)
         simulation_button.pack(pady=10)
 
         # Create button to open quiz file
-        quiz_button = tk.Button(self.root, text="Quiz", command=self.open_quiz, font=("Times New Roman", 16))
+        quiz_button = tk.Button(self.root, text="Quiz", command=self.open_quiz, font=("Times New Roman", 16), width=16, height=2)
         quiz_button.pack(pady=10)
 
         # Create button to quit program
-        quit_button = tk.Button(self.root, text="Quit", command=self.quit_program, font=("Times New Roman", 16))
+        quit_button = tk.Button(self.root, text="Quit", command=self.quit_program, font=("Times New Roman", 16), width=16, height=2)
         quit_button.pack(pady=10)
 
     def open_theory(self):
         import theory
+        theory = theory.TheoryGUI(self.root)
+        theory.mainloop()
 
     def open_simulation(self):
         import simulation
